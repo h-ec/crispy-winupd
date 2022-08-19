a883e71e1b15cd81db1e5e54dae455113ec65d1c
Suffected <99141995+m-ve@users.noreply.github.com>
2022-08-19 17:34:40 +0200

    Added Files Setup batch

---
+++ project-files-setup.bat
@@ -0,0 +1,15 @@
+@ ECHO Making default files
+mkdir final-dest
+mkdir bin
+@ ECHO Creating dummy files in both directories
+echo "dummy text" > final-dest/dummy.txt
+echo "dummy text" > bin/dummy.txt
+@ ECHO DONE.
+@ ECHO DONE..
+@ ECHO DONE...
+@ ECHO [ / ] Pausing batch file.
+@ ECHO [ - ] Pausing batch file..
+@ ECHO [ \ ] Pausing batch file...
+@ ECHO [ # ] Pausing batch file.
+@ ECHO Press any key to exit "project-files-setup.bat"
+@ PAUSE
